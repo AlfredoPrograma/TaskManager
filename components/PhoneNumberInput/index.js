fd37b8dda448ad7b2c08254bc3c5d8f3ddf30ed6
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2022-04-10 20:26:03 -0400

    [feat]: Add reusable PhoneNumberInput  component

---
+++ components/PhoneNumberInput/index.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import { useFormContext } from 'react-hook-form'
+import MuiPhoneNumber from 'material-ui-phone-number'
+import validator from 'validator'
+
+export const PhoneNumberInput = ({ countriesAvailable, regionsAvailable, defaultCountry, name, rules }) => {
+  const { register, getValues, setValue, formState: { errors } } = useFormContext()
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const currentPhoneNumber = getValues(name)
+
+  const validatePhoneNumber = (phone) => validator.isMobilePhone(phone) || 'Invalid phone number'
+  const handleChange = (value) => setValue(name, value, { shouldDirty: true, shouldValidate: true })
+
+  useEffect(() => {
+    register(name, { ...rules, validate: validatePhoneNumber })
+  }, [register])
+
+  useEffect(() => {
+    setPhoneNumber(currentPhoneNumber || '')
+  }, [currentPhoneNumber, setPhoneNumber])
+
+  return (
+    <MuiPhoneNumber
+      variant='outlined'
+      onlyCountries={countriesAvailable}
+      regions={regionsAvailable}
+      countryCodeEditable={false}
+      value={phoneNumber}
+      onChange={handleChange}
+      defaultCountry={defaultCountry}
+      FormHelperTextProps={{ style: { margin: 0 } }}
+      helperText={ errors[name]?.message }
+      error={!!errors[name]?.message}
+    />
+  )
+}
