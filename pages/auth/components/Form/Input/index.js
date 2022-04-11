3ba34e22612454107c921b4cdfaf7283e7bd24a9
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2022-04-10 20:28:22 -0400

    [fix]: Add toggable autoFocus prop to FormInput component

--- pages/auth/components/Form/Input/index.js
+++ pages/auth/components/Form/Input/index.js
@@ -1,15 +1,13 @@
 import { TextField } from '@mui/material'
 import { useFormContext } from 'react-hook-form'
 
-export function FormInput ({ label, type, placeholder, name, rules, fullWidth, variant }) {
+export function FormInput ({ label, type, placeholder, name, rules, fullWidth, variant, autoFocus }) {
   const { register, formState: { errors } } = useFormContext()
-
-  console.log(errors[name])
 
   return (
     <>
       <TextField
-        autoFocus
+        autoFocus={autoFocus}
         autoComplete='off'
         fullWidth={fullWidth}
         variant={variant ?? 'outlined'}
@@ -19,7 +17,7 @@ export function FormInput ({ label, type, placeholder, name, rules, fullWidth, v
         label={label}
         helperText={ errors[name]?.message }
         FormHelperTextProps={{ style: { margin: 0 } }}
-        error={errors[name]?.message}
+        error={!!errors[name]?.message}
         {...register(name, rules)}
       />
     </>
